d33efc270e73ec80ad57646cedbf78c82b40b2dc
kevin.le2 <kevin.le2@veeva.com>
2025-06-15 18:30:08 -0700

    added bottom underline border styles

--- src/sections/Works.tsx
+++ src/sections/Works.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const works = [
   {
     title: "lekev.in",
@@ -25,20 +23,21 @@ const Works = () => {
         <span className="font-base text-red-600">W</span>orks
       </div>
       <div className="flex flex-wrap justify-center items-start lg:border-t-[1px]">
-        {works.map((work, idx) => (
+        {works.map((work, index) => (
           <a
-            key={idx}
+            key={index}
             href={work.href}
             target="_blank"
             rel="noopener noreferrer"
-            className={[
-              "group flex flex-col items-start space-y-2 pt-8",
-              idx !== 0 && "lg:border-l lg:border-white lg:pl-8 lg:ml-8",
-            ]
-              .filter(Boolean)
-              .join(" ")}
+            className=
+              "group relative flex flex-col items-start space-y-2 pt-8 lg:pl-8 lg:ml-8"
+            
           >
-            <div className="w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] rounded overflow-hidden hover:border-red-900 transition-all">
+            {index !== 0 && (
+              <span className="absolute top-6 bottom-6 -left-4 w-px bg-white opacity-50 hidden lg:block" />
+            )}
+
+            <div className="w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] rounded transition-all">
               <img
                 src={work.img}
                 alt={work.title}
@@ -48,6 +47,7 @@ const Works = () => {
             <p className="text-xl text-zinc-400 group-hover:text-white transition-all">
               {work.title}
             </p>
+            <span className="absolute inset-x-0 bottom-0 h-[1px] bg-white origin-left scale-x-0 transition-transform duration-500 group-hover:scale-x-100" />
           </a>
         ))}
       </div>
@@ -56,18 +56,12 @@ const Works = () => {
         href="https://github.com/lekevin"
         target="_blank"
         rel="noopener noreferrer"
-        className="mt-10 opacity-70 hover:opacity-100 transition duration-300"
+        className="opacity-70 transition duration-300 pt-20 "
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          viewBox="0 0 24 24"
-          fill="white"
-          className="hover:scale-110 transition-transform duration-300"
-        >
-          <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.09 3.29 9.4 7.86 10.94.58.1.79-.25.79-.56v-2.17c-3.2.7-3.88-1.52-3.88-1.52-.53-1.34-1.3-1.7-1.3-1.7-1.06-.72.08-.7.08-.7 1.18.08 1.8 1.22 1.8 1.22 1.04 1.78 2.72 1.27 3.38.97.1-.76.41-1.27.74-1.56-2.56-.29-5.26-1.28-5.26-5.7 0-1.26.45-2.29 1.19-3.09-.12-.3-.52-1.5.11-3.13 0 0 .98-.31 3.2 1.18a11.1 11.1 0 0 1 2.91-.39c.99 0 1.99.13 2.91.39 2.22-1.5 3.19-1.18 3.19-1.18.63 1.63.23 2.83.11 3.13.74.8 1.18 1.83 1.18 3.09 0 4.42-2.7 5.4-5.27 5.7.42.36.79 1.08.79 2.18v3.23c0 .31.21.67.8.56A10.99 10.99 0 0 0 23.5 12c0-6.35-5.15-11.5-11.5-11.5z" />
-        </svg>
+        <img
+          src="src/assets/github.png"
+          className="w-12 h-12 invert brightness-0 hover:opacity-70 hover:scale-95 transition-transform duration-500 cursor-pointer"
+        />
       </a>
     </div>
   );
